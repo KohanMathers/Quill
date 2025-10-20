6bf8f033920114cccaa8d6dfbeb45d8338ea6040
Kohan Mathers <kmathers@kmathers.co.uk>
2025-10-20 11:37:18 +0100

    Add lexer and debug output to plugin logger

--- src/main/java/me/kmathers/quill/Quill.java
+++ src/main/java/me/kmathers/quill/Quill.java
@@ -1,11 +1,33 @@
 package me.kmathers.quill;
 
+import java.util.List;
+
 import org.bukkit.plugin.java.JavaPlugin;
+
+import me.kmathers.quill.lexer.QuillLexer;
+import me.kmathers.quill.lexer.QuillLexer.Token;
 
 public class Quill extends JavaPlugin {
     @Override
     public void onEnable() {
         getLogger().info("Enabling Quill...");
+        getLogger().info("========Quill Lexer Test========");
+        test();
+        getLogger().info("================================");
+    }
+
+    private void test() {
+        try {
+            String sourceCode = "let x = 27;";
+            QuillLexer lexer = new QuillLexer(sourceCode);
+            List<Token> tokens = lexer.tokenize();
+            for (Token token: tokens) {
+                getLogger().info(token.kind.toString());
+            }
+        } catch (QuillLexer.LexerException e) {
+            getLogger().severe("Lexer error: " + e.getMessage());
+            return;
+        }
     }
 
     @Override

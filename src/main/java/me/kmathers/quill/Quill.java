f2296bb807780cea7529321584f1f18281e5fee9
Kohan Mathers <kmathers@kmathers.co.uk>
2025-10-20 14:00:01 +0100

    Add parser

--- src/main/java/me/kmathers/quill/Quill.java
+++ src/main/java/me/kmathers/quill/Quill.java
@@ -6,26 +6,55 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import me.kmathers.quill.lexer.QuillLexer;
 import me.kmathers.quill.lexer.QuillLexer.Token;
+import me.kmathers.quill.parser.AST.ASTNode;
+import me.kmathers.quill.parser.AST.Program;
+import me.kmathers.quill.parser.QuillParser;
 
 public class Quill extends JavaPlugin {
     @Override
     public void onEnable() {
         getLogger().info("Enabling Quill...");
-        getLogger().info("========Quill Lexer Test========");
-        test();
-        getLogger().info("================================");
+        getLogger().info("===========Quill Tests===========");
+        getLogger().info("              Lexer");
+        getLogger().info("---------------------------------");
+        testLexer();
+        getLogger().info("---------------------------------");
+        getLogger().info("              Parser");
+        getLogger().info("---------------------------------");
+        testParser();
+        getLogger().info("=================================");
     }
 
-    private void test() {
+    private void testLexer() {
         try {
             String sourceCode = "let x = 27;";
             QuillLexer lexer = new QuillLexer(sourceCode);
             List<Token> tokens = lexer.tokenize();
             for (Token token: tokens) {
-                getLogger().info(token.kind.toString());
+                getLogger().info(token.value + "| " + token.kind.toString() + " | " + token.line + ", " + token.column);
             }
         } catch (QuillLexer.LexerException e) {
             getLogger().severe("Lexer error: " + e.getMessage());
+            return;
+        }
+    }
+
+    private void testParser() {
+        try {
+            String sourceCode = "let x = 27;";
+            QuillLexer lexer = new QuillLexer(sourceCode);
+            List<Token> tokens = lexer.tokenize();
+            QuillParser parser = new QuillParser(tokens);
+            Program ast = parser.parse();
+            List<ASTNode> statements = ast.statements;
+            for (ASTNode statement: statements) {
+                getLogger().info(statement.line + ", " + statement.column);
+            }
+        } catch (QuillLexer.LexerException e) {
+            getLogger().severe("Lexer error: " + e.getMessage());
+            return;
+        } catch (QuillParser.ParseException e) {
+            getLogger().severe("Parser error: " + e.getMessage());
             return;
         }
     }
